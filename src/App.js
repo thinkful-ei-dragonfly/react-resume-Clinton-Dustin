890728cafb67a59696711c9559467b95a27dab82
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-20 11:46:22 -0400

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <ul>
+          <li>Name: Sally Student</li>
+          <li>Email: Sally@gmail.com</li>
+          <li>Phone Number: 555-555-5555</li>
+          <li>
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg">
+            </img>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <p>Education: Graduated from the University of Nebraska in 2013</p>
+      </section>
+      <section>
+        <h2>Employment History</h2>
+        <ul>
+          <li>5 years as a Bartender</li>
+          <li>3 years as a Bar manager</li>
+        </ul>
+      </section>
     </div>
   );
 }
